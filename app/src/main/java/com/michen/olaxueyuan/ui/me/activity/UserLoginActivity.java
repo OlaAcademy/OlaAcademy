b603b1a63f76ea007f12f1067c3da2a4f91e70ea
mingge <mingge@126.com>
2017-04-04 22:13:02 +0800

    微信第三方登陆（未完成）

--- app/src/main/java/com/michen/olaxueyuan/ui/me/activity/UserLoginActivity.java
+++ app/src/main/java/com/michen/olaxueyuan/ui/me/activity/UserLoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +15,7 @@ import com.avos.avoscloud.im.v2.callback.AVIMClientCallback;
 import com.michen.olaxueyuan.R;
 import com.michen.olaxueyuan.app.SEAPP;
 import com.michen.olaxueyuan.app.SEConfig;
+import com.michen.olaxueyuan.common.manager.Logger;
 import com.michen.olaxueyuan.common.manager.ToastUtil;
 import com.michen.olaxueyuan.protocol.manager.HomeListManager;
 import com.michen.olaxueyuan.protocol.manager.SEAuthManager;
@@ -22,7 +24,21 @@ import com.michen.olaxueyuan.protocol.result.TokenInfoResult;
 import com.michen.olaxueyuan.protocol.result.UserLoginNoticeModule;
 import com.michen.olaxueyuan.ui.activity.SEBaseActivity;
 import com.michen.olaxueyuan.ui.circle.chat.CustomUserProvider;
+import com.michen.olaxueyuan.ui.course.pay.weixin.Constants;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.controller.listener.SocializeListeners;
+import com.umeng.socialize.exception.SocializeException;
+import com.umeng.socialize.sso.UMSsoHandler;
+import com.umeng.socialize.weixin.controller.UMWXHandler;
 
+import java.util.Map;
+import java.util.Set;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.leancloud.chatkit.LCChatKit;
 import cn.leancloud.chatkit.LCChatKitUser;
 import de.greenrobot.event.EventBus;
@@ -32,6 +48,8 @@ import retrofit.client.Response;
 
 public class UserLoginActivity extends SEBaseActivity {
 
+    @Bind(R.id.login_weixin)
+    ImageView loginWeixin;
     private EditText phoneET;
     private EditText passET;
     private Button loginBtn;
@@ -39,12 +57,19 @@ public class UserLoginActivity extends SEBaseActivity {
 
     public static int PASS_FOREGT = 0x0101;
     public static int USER_REG = 0x0202;
+    private UMSocialService mController;
+    private String source;
+    private String sourceId;
+    private String unionId = "";
+    private String headUrl;
+    private String gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
-
+        ButterKnife.bind(this);
+        mController = UMServiceFactory.getUMSocialService("com.umeng.login");
         setTitleText("登录欧拉学院");
         setLeftImageInvisibility();
         setRightImageInvisibility();
@@ -190,6 +215,86 @@ public class UserLoginActivity extends SEBaseActivity {
             phoneET.setText(data.getStringExtra("phone"));
             passET.setText(data.getStringExtra("password"));
             login();
+        }
+        UMSsoHandler ssoHandler = mController.getConfig().getSsoHandler(requestCode);
+        if (ssoHandler != null) {
+            ssoHandler.authorizeCallBack(requestCode, resultCode, data);
+        }
+    }
+
+    private void thirdLoginWeChat() {
+        // 添加微信平台
+        UMWXHandler wxHandler = new UMWXHandler(UserLoginActivity.this, Constants.APP_ID, Constants.API_KEY);
+        wxHandler.addToSocialSDK();
+        mController.doOauthVerify(UserLoginActivity.this, SHARE_MEDIA.WEIXIN, new SocializeListeners.UMAuthListener() {
+            @Override
+            public void onStart(SHARE_MEDIA platform) {
+                Toast.makeText(UserLoginActivity.this, "授权开始", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(SocializeException e, SHARE_MEDIA platform) {
+                Toast.makeText(UserLoginActivity.this, "授权错误", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onComplete(final Bundle value, SHARE_MEDIA platform) {
+                Toast.makeText(UserLoginActivity.this, "授权完成", Toast.LENGTH_SHORT).show();
+                //获取相关授权信息
+                mController.getPlatformInfo(UserLoginActivity.this, SHARE_MEDIA.WEIXIN, new SocializeListeners.UMDataListener() {
+                    @Override
+                    public void onStart() {
+                        Toast.makeText(UserLoginActivity.this, "获取平台数据开始...", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onComplete(int status, Map<String, Object> info) {
+                        if (status == 200 && info != null) {
+                            StringBuilder sb = new StringBuilder();
+                            Set<String> keys = info.keySet();
+                            for (String key : keys) {
+                                sb.append(key + "=" + info.get(key).toString() + "\r\n");
+                                if ("headimgurl".equals(key)) {
+                                    headUrl = info.get(key).toString();
+                                }
+                                if ("sex".equals(key)) {
+                                    if ("1".equals(info.get(key).toString())) {
+                                        gender = "男";
+                                    } else if ("2".equals(info.get(key).toString())) {
+                                        gender = "女";
+                                    } else {
+                                        gender = "";
+                                    }
+                                }
+                                if ("unionid".equals(key)) {
+                                    unionId = info.get(key).toString();
+                                }
+                            }
+                            source = "wechat";
+                            sourceId = value.getString("uid");
+                            Logger.e("uid======================================" + value.getString("uid"));
+//                            bindingPhoneOrNot(source, unionId, sourceId);
+                            Logger.e("==========" + sb.toString());
+                        } else {
+                            Logger.e("==========发生错误：" + status);
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancel(SHARE_MEDIA platform) {
+                Toast.makeText(UserLoginActivity.this, "授权取消", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @OnClick({R.id.login_weixin})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_weixin:
+                thirdLoginWeChat();
+                break;
         }
     }
 }
